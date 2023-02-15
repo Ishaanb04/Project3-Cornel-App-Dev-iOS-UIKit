a8ae37bdf1ecd58830361f31ca5b0877d3e3b7e5
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2023-02-15 16:09:22 -0500

    completed pushvc and added delegate methods

--- Project3CornellAppDev/ContactViewController.swift
+++ Project3CornellAppDev/ContactViewController.swift
@@ -8,17 +8,37 @@
 import UIKit
 
 class ContactViewController: UIViewController {
-
+    
+    weak var del: EditProfileDelegate?
+    
     var nameLabel = UILabel()
     var nameTextField = TextFieldWithPadding()
     var portfolioLabel = UILabel()
     var portfolioTextfield = TextFieldWithPadding()
     var yearLabel = UILabel()
     var yearSlider = UISlider()
+    var currentYear = UILabel()
     var aboutLabel = UILabel()
-    var aboutTextField = TextFieldWithPadding()
+    var aboutTextField = UITextView()
     
-    var currentName = ""
+    var currentName: String
+    var currentPortFolio: String
+    var currentAbout: String
+    var currentYearValue: Int
+    
+    init(del: EditProfileDelegate, currentName: String = "", currentPortFolio: String = "", currentAbout: String = "", currentYearValue: Int = 1) {
+        self.currentName = currentName
+        self.currentPortFolio = currentPortFolio
+        self.currentAbout = currentAbout
+        self.currentYearValue = currentYearValue
+        self.del = del
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +51,8 @@ class ContactViewController: UIViewController {
         view.addSubview(nameLabel)
         
         
-        nameTextField.placeholder = currentName
+        nameTextField.placeholder = currentName == "" ? currentName : "Enter Your Name"
+        nameTextField.text = currentName
         nameTextField.layer.cornerRadius = 25
         nameTextField.backgroundColor =  UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
         nameTextField.clipsToBounds = true
@@ -43,7 +64,8 @@ class ContactViewController: UIViewController {
         portfolioLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(portfolioLabel)
         
-        portfolioTextfield.placeholder = currentName
+        portfolioTextfield.placeholder = currentPortFolio == "" ? currentPortFolio : "Enter Your Portfolio"
+        portfolioTextfield.text = currentPortFolio
         portfolioTextfield.layer.cornerRadius = 25
         portfolioTextfield.backgroundColor =  UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
         portfolioTextfield.clipsToBounds = true
@@ -57,6 +79,7 @@ class ContactViewController: UIViewController {
         
         yearSlider.minimumValue = 1
         yearSlider.maximumValue = 5
+        yearSlider.value = Float(currentYearValue)
         yearSlider.addTarget(self, action: #selector(self.sliderValueDidChange(_:)), for: .valueChanged)
         yearSlider.isContinuous = false
         yearSlider.backgroundColor = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
@@ -65,53 +88,110 @@ class ContactViewController: UIViewController {
         yearSlider.layer.cornerRadius = 25
         view.addSubview(yearSlider)
         
+        switch currentYearValue{
+        case 1:
+            currentYear.text = "Freshmen"
+        case 2:
+            currentYear.text = "Sophomore"
+        case 3:
+            currentYear.text = "Junior"
+        case 4:
+            currentYear.text = "Senior"
+        case 5:
+            currentYear.text = "Super Senior"
+        default:
+            currentYear.text = "Freshmen"
+        }
+        currentYear.translatesAutoresizingMaskIntoConstraints = false
+        currentYear.font = UIFont(name: "Roboto-Regular", size: 12)
+        currentYear.textColor = UIColor(red: 0.708, green: 0.708, blue: 0.708, alpha: 1)
+        view.addSubview(currentYear)
+        
+        aboutLabel.text = "ABOUT"
+        aboutLabel.textColor = UIColor(red: 0.708, green: 0.708, blue: 0.708, alpha: 1)
+        aboutLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(aboutLabel)
+        aboutTextField.text = currentAbout
+        aboutTextField.layer.cornerRadius = 25
+        aboutTextField.backgroundColor =  UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+        aboutTextField.contentInset = UIEdgeInsets(top: 10, left: 25, bottom: 10, right: 25)
+        aboutTextField.font = UIFont(name: "Roboto-Regular", size: 12)
+        aboutTextField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(aboutTextField)
+        
+        let savebutton = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveAndExit))
+        self.navigationItem.rightBarButtonItem = savebutton
         setUpConstraints()
+    }
+    
+    @objc func saveAndExit(){
+        del?.changeYear(val: currentYearValue)
+        del?.changeName(str: nameTextField.text ?? "")
+        del?.changePortfolio(str: portfolioTextfield.text ?? "")
+        del?.aboutFunc(str: aboutTextField.text ?? "")
+        navigationController?.popViewController(animated: true)
     }
     
     @objc func sliderValueDidChange(_ sender:UISlider!)
     {
-        print("Slider value changed")
         
         // Use this code below only if you want UISlider to snap to values step by step
         let roundedStepValue = round(sender.value / 1) * 1
         sender.value = roundedStepValue
         
-        print("Slider step value \(Int(roundedStepValue))")
+        currentYearValue = Int(roundedStepValue)
+        switch roundedStepValue{
+            
+        case 1:
+            currentYear.text = "Freshmen"
+        case 2:
+            currentYear.text = "Sophomore"
+        case 3:
+            currentYear.text = "Junior"
+        case 4:
+            currentYear.text = "Senior"
+        case 5:
+            currentYear.text = "Super Senior"
+        default:
+            currentYear.text = "Freshmen"
+        }
+        
+        
     }
     
     func setUpConstraints(){
         NSLayoutConstraint.activate([
             nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25),
-            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 35),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
             nameLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
         
         NSLayoutConstraint.activate([
-           nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
-           nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-           nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-           nameTextField.heightAnchor.constraint(equalToConstant: 40)
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            nameTextField.heightAnchor.constraint(equalToConstant: 40)
         ])
         
         NSLayoutConstraint.activate([
-           portfolioLabel.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 25),
-           portfolioLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-           portfolioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 35),
-           portfolioLabel.heightAnchor.constraint(equalToConstant: 20)
+            portfolioLabel.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 25),
+            portfolioLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            portfolioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            portfolioLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
         
         NSLayoutConstraint.activate([
-          portfolioTextfield.topAnchor.constraint(equalTo: portfolioLabel.bottomAnchor, constant: 5),
-          portfolioTextfield.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-          portfolioTextfield.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-          portfolioTextfield.heightAnchor.constraint(equalToConstant: 40)
+            portfolioTextfield.topAnchor.constraint(equalTo: portfolioLabel.bottomAnchor, constant: 5),
+            portfolioTextfield.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            portfolioTextfield.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            portfolioTextfield.heightAnchor.constraint(equalToConstant: 40)
         ])
         
         NSLayoutConstraint.activate([
             yearLabel.topAnchor.constraint(equalTo: portfolioTextfield.bottomAnchor, constant: 25),
-            yearLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            yearLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 35),
+            yearLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            yearLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
             yearLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
         
@@ -121,30 +201,58 @@ class ContactViewController: UIViewController {
             yearSlider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
             yearSlider.heightAnchor.constraint(equalToConstant: 50)
         ])
+        
+        NSLayoutConstraint.activate([
+            currentYear.topAnchor.constraint(equalTo: yearSlider.bottomAnchor, constant: 5),
+            currentYear.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currentYear.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            aboutLabel.topAnchor.constraint(equalTo: currentYear.topAnchor, constant: 25),
+            aboutLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            aboutLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            aboutLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            aboutTextField.topAnchor.constraint(equalTo: aboutLabel.bottomAnchor, constant: 5),
+            aboutTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            aboutTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            aboutTextField.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 10)
+        ])
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 class TextFieldWithPadding: UITextField{
     var customPadding = UIEdgeInsets(top: 12, left: 30, bottom: 12, right: 30)
     
     override func textRect(forBounds bounds: CGRect) -> CGRect {
-            let rect = super.textRect(forBounds: bounds)
-            return rect.inset(by: customPadding)
-        }
+        let rect = super.textRect(forBounds: bounds)
+        return rect.inset(by: customPadding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let rect = super.editingRect(forBounds: bounds)
+        return rect.inset(by: customPadding)
+    }
+}
 
-        override func editingRect(forBounds bounds: CGRect) -> CGRect {
-            let rect = super.editingRect(forBounds: bounds)
-            return rect.inset(by: customPadding)
-        }
+
+protocol EditProfileDelegate: UIViewController{
+    func changeName(str: String)
+    func changePortfolio(str: String)
+    func changeYear(val: Int)
+    func aboutFunc(str: String)
 }
--- Project3CornellAppDev/ViewController.swift
+++ Project3CornellAppDev/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
     var imageName = ["Avatar1", "Avatar2", "Avatar3", "Avatar4", "Avatar5", "Avatar6"]
     var currentIndex = 0
     
+    var currentName = "Add Name"
+    var currentPortFolio = "sample.com"
+    var currentYear = 1
+    var currentAbout = "About Me........"
+    
     var editbutton = UIButton()
     
     override func viewDidLoad() {
@@ -48,6 +53,57 @@ class ViewController: UIViewController {
         editbutton.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
         editbutton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(editbutton)
+        
+        nameLabel.text = currentName
+        nameLabel.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        nameLabel.font = UIFont(name: "Roboto-Regular", size: 24)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        portfolioLabel.text = currentPortFolio
+        portfolioLabel.textColor = UIColor(red: 0.708, green: 0.708, blue: 0.708, alpha: 1)
+        portfolioLabel.font = UIFont(name: "Roboto-Regular", size: 12)
+        portfolioLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(portfolioLabel)
+        
+        yearLabel.text = "YEAR"
+        yearLabel.textColor = UIColor(red: 0.708, green: 0.708, blue: 0.708, alpha: 1)
+        yearLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(yearLabel)
+        
+        switch currentYear{
+        case 1:
+            year.text = "Freshmen"
+        case 2:
+            year.text = "Sophomore"
+        case 3:
+            year.text = "Junior"
+        case 4:
+            year.text = "Senior"
+        case 5:
+            year.text = "Super Senior"
+        default:
+            year.text = "Freshmen"
+        }
+        year.translatesAutoresizingMaskIntoConstraints = false
+        year.font = UIFont(name: "Roboto-Regular", size: 12)
+        year.textAlignment = .center
+        year.textColor = UIColor(red: 0.708, green: 0.708, blue: 0.708, alpha: 1)
+        view.addSubview(year)
+        
+        aboutLabel.text = "ABOUT"
+        aboutLabel.textColor = UIColor(red: 0.708, green: 0.708, blue: 0.708, alpha: 1)
+        aboutLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(aboutLabel)
+        
+        about.layer.cornerRadius = 25
+        about.backgroundColor =  UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+        about.contentInset = UIEdgeInsets(top: 10, left: 25, bottom: 10, right: 25)
+        about.font = UIFont(name: "Roboto-Regular", size: 16)
+        about.translatesAutoresizingMaskIntoConstraints = false
+        about.isEditable = false
+        view.addSubview(about)
+        
         setUpConstraints()
         
         
@@ -59,29 +115,69 @@ class ViewController: UIViewController {
     }
     
     @objc func editButtonTapped(){
-        let contactVC = ContactViewController()
-        navigationController?.pushViewController(contactVC, animated: true)
+        navigationController?.pushViewController(ContactViewController(del: self, currentName: currentName, currentPortFolio: currentPortFolio, currentAbout: currentAbout, currentYearValue: currentYear), animated: true)
     }
     
     func setUpConstraints(){
         NSLayoutConstraint.activate([
-            profilePicture.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            profilePicture.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             profilePicture.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             profilePicture.heightAnchor.constraint(equalToConstant: 200),
             profilePicture.widthAnchor.constraint(equalToConstant: 200)
         ])
         
         NSLayoutConstraint.activate([
-            profilepictureMask.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            profilepictureMask.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             profilepictureMask.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             profilepictureMask.heightAnchor.constraint(equalToConstant: 220),
             profilepictureMask.widthAnchor.constraint(equalToConstant: 220)
         ])
         
         NSLayoutConstraint.activate([
-           editbutton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-           editbutton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+            nameLabel.topAnchor.constraint(equalTo: profilepictureMask.bottomAnchor, constant: 10),
+            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
+        
+        NSLayoutConstraint.activate([
+            portfolioLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),
+            portfolioLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            portfolioLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            yearLabel.topAnchor.constraint(equalTo: portfolioLabel.bottomAnchor, constant: 20),
+            yearLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            yearLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            yearLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            year.topAnchor.constraint(equalTo: yearLabel.bottomAnchor, constant: 5),
+            year.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            year.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            year.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            aboutLabel.topAnchor.constraint(equalTo: year.bottomAnchor, constant: 25),
+            aboutLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            aboutLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            aboutLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            about.topAnchor.constraint(equalTo: aboutLabel.bottomAnchor, constant: 5),
+            about.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            about.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            about.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -60)
+        ])
+        
+        NSLayoutConstraint.activate([
+            editbutton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            editbutton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+        
     }
     
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
@@ -101,4 +197,39 @@ extension ViewController: MainImageChangeDelgate{
     }
 }
 
+extension ViewController: EditProfileDelegate {
+    func aboutFunc(str: String) {
+        currentAbout = str
+        about.text = str
+    }
+    
+    func changeName(str: String) {
+        currentName = str
+        nameLabel.text = str
+    }
+    
+    func changePortfolio(str: String) {
+        currentPortFolio = str
+        portfolioLabel.text = str
+    }
+    
+    func changeYear(val: Int) {
+        currentYear = val
+        switch currentYear{
+        case 1:
+            year.text = "Freshmen"
+        case 2:
+            year.text = "Sophomore"
+        case 3:
+            year.text = "Junior"
+        case 4:
+            year.text = "Senior"
+        case 5:
+            year.text = "Super Senior"
+        default:
+            year.text = "Freshmen"
+        }
+    }
+    
+}
 
